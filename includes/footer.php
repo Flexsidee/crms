206988416306646ad01307233cda44dfec49b3b2
Daniel Somade <flexsidee@gmail.com>
2024-04-28 06:23:08 +0100

    cleaned up the templates

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+   <div class="footer">
+     <div class="copyright">
+       <p>Copyright © Designed &amp; Developed by <a href="https://linktr.ee/daniel_somade" target="_blank">Somade Daniel</a> 2024</p>
+     </div>
+   </div>
---
+++ includes/sidebar.php
@@ -0,0 +1,63 @@
+<div class="quixnav">
+  <div class="quixnav-scroll">
+    <ul class="metismenu" id="menu">
+      <li class="nav-label first">Main Menu</li>
+      <li>
+        <a href="index.php" aria-expanded="false">
+          <i class="icon icon-app-store"></i>
+          <span class="nav-text">Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+          <i class="icon icon-single-copy-06"></i>
+          <span class="nav-text">Courses</span>
+        </a>
+        <ul aria-expanded="false">
+          <li><a href="#">Add Course</a></li>
+          <li><a href="#">Manage Courses</a></li>
+        </ul>
+      </li>
+      <li>
+        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+          <i class="icon icon-single-04"></i>
+          <span class="nav-text"> Students</span>
+        </a>
+        <ul aria-expanded="false">
+          <li><a href="#">Add Students</a></li>
+          <li><a href="#">Manage Students</a></li>
+        </ul>
+      </li>
+      <li>
+        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+          <i class="icon icon-single-04"></i>
+          <span class="nav-text"> Lecturers</span>
+        </a>
+        <ul aria-expanded="false">
+          <li><a href="#">Add Lecturers</a></li>
+          <li><a href="#">Manage Lecturers</a></li>
+        </ul>
+      </li>
+
+      <li class="nav-label">Results</li>
+      <li>
+        <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+          <i class="icon icon-layout-25"></i>
+          <span class="nav-text"> Results</span>
+        </a>
+        <ul aria-expanded="false">
+          <li><a href="#">Add Results</a></li>
+          <li><a href="#">Manage Results</a></li>
+        </ul>
+      </li>
+
+      <li class="nav-label">Profile</li>
+      <li>
+        <a href="profile.php" aria-expanded="false">
+          <i class="icon icon-single-04"></i>
+          <span class="nav-text">Profile</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
