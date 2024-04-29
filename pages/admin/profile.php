2005c4bce6279b51950747dddfad8ba6308a5305
Daniel Somade <flexsidee@gmail.com>
2024-04-29 04:00:55 +0100

    admin and superadmin can see courses list

--- pages/admin/profile.php
+++ pages/admin/profile.php
@@ -2,7 +2,7 @@
 session_start();
 
 //check if student is logged in
-if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 1) {
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] > 1) {
     //Redirect to login page if not logged in
     header("Location: ../../index.php?error=You%20need%20to%20login%20to%20acces%20this%20website");
     exit();
