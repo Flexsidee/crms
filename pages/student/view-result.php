ddd66555debe3cb7dc49ee88b1d9129b81eef816
Daniel Somade <flexsidee@gmail.com>
2024-06-18 19:48:00 +0100

    create html for students view result

--- pages/student/view-result.php
+++ pages/student/view-result.php
@@ -50,6 +50,40 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 4) {
   <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
   <link href="../../assets/css/style.css" rel="stylesheet">
   <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/caleb.png">
+  <style>
+    .top {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .resultTable {
+      width: 100%;
+      border: 1px grey solid;
+    }
+
+    .resultTable th {
+      background-color: #f1f1f1;
+      border: 1px grey solid;
+    }
+
+    .resultTable td {
+      border: 1px grey solid;
+    }
+
+    .resultTable td,
+    .resultTable th {
+      color: black;
+      padding: 10px;
+    }
+
+    .info {
+      /* margin-top: 30px; */
+      margin-bottom: 0;
+      color: black;
+      font-size: 120%;
+    }
+  </style>
 
 </head>
 
@@ -99,11 +133,157 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 4) {
     <div class="content-body">
       <div class="container-fluid">
 
-        <div class="row">
+        <div class="row" id="printableContent">
           <div class="col-lg-12">
             <div class="card">
-              <div class="card-body">
-
+              <div class="card-body" style="padding: 0">
+                <div class="top px-3 pt-3">
+                  <p class="info"><strong>
+                      Registered course(s) and scores for 2022/2023 First semester
+                    </strong>
+                  </p>
+                  <p class="info">Switch Semester: &nbsp;
+                    <select style="padding: 5px; font-size: 90%">
+                      <option>2021/2022 First</option>
+                      <option>2021/2022 Second</option>
+                      <option>2022/2023 First</option>
+                      <option>2022/2023 Second</option>
+                      <option>2023/2024 First</option>
+                      <option>2023/2024 Second</option>
+                    </select>
+                  </p>
+                </div>
+                <hr />
+                <div class="px-4">
+                  <p class="info">Kindly view your scores below
+                  </p>
+                  <table class="resultTable">
+                    <thead>
+                      <tr>
+                        <th>Course Name</th>
+                        <th>Course Code</th>
+                        <th>Score</th>
+                        <th>Point</th>
+                        <th>Grade</th>
+                        <th>Unit</th>
+                        <th>Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Architecture and Organization I</td>
+                        <td>CSC 301</td>
+                        <td>74</td>
+                        <td>5</td>
+                        <td>A</td>
+                        <td>3</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Structured Programming</td>
+                        <td>CSC 301</td>
+                        <td>73</td>
+                        <td>5</td>
+                        <td>A</td>
+                        <td>3</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Object-Oriented Programming</td>
+                        <td>CSC 335</td>
+                        <td>74</td>
+                        <td>5</td>
+                        <td>A</td>
+                        <td>3</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Systems Analysis and Design</td>
+                        <td>CSC 311</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>F</td>
+                        <td>4</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Data Management I</td>
+                        <td>CSC 331</td>
+                        <td>77</td>
+                        <td>5</td>
+                        <td>A</td>
+                        <td>3</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Character in Leadership III</td>
+                        <td>GST 311</td>
+                        <td>88</td>
+                        <td>5</td>
+                        <td>A</td>
+                        <td>1</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Computational Science and Numerical Methods</td>
+                        <td>MTH 301</td>
+                        <td>80</td>
+                        <td>5</td>
+                        <td>A</td>
+                        <td>3</td>
+                        <td>C</td>
+                      </tr>
+                      <tr>
+                        <td>Introductory to Entrepreneurial Studies</td>
+                        <td>GST 301</td>
+                        <td>73</td>
+                        <td>5</td>
+                        <td>A</td>
+                        <td>2</td>
+                        <td>C</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <div class="col-lg-4 px-4 mt-5 mb-5">
+                  <p class="info">Kindly see results summary below</p>
+                  <table class="resultTable">
+                    <thead>
+                      <tr>
+                        <th></th>
+                        <th>TCP</th>
+                        <th>TNU</th>
+                        <th>TNUP</th>
+                        <th>GPA</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td> <strong>Current</strong></td>
+                        <td>102</td>
+                        <td>25</td>
+                        <td>21</td>
+                        <td>4.08</td>
+                      </tr>
+                      <tr>
+                        <td><strong>Previous</strong></td>
+                        <td>198</td>
+                        <td>44</td>
+                        <td>0</td>
+                        <td>4.5</td>
+                      </tr>
+                      <tr>
+                        <td><strong>Cummulative</strong></td>
+                        <td>300</td>
+                        <td>69</td>
+                        <td>63</td>
+                        <td>4.35</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <hr />
+                <button class="btn btn-secondary ml-4 mb-4 mt-2" onclick="printDiv()">Print Result</button>
               </div>
             </div>
           </div>
@@ -135,6 +315,18 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 4) {
   <script src="../../assets/vendor/global/global.min.js"></script>
   <script src="../../assets/js/quixnav-init.js"></script>
   <script src="../../assets/js/custom.min.js"></script>
+  <script>
+    function printDiv() {
+      var printContents = document.getElementById('printableContent').innerHTML;
+      var originalContents = document.body.innerHTML;
+
+      document.body.innerHTML = printContents;
+
+      window.print();
+
+      document.body.innerHTML = originalContents;
+    }
+  </script>
 
 
 </body>
